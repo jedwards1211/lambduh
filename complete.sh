5c1c0cd9ef154fdcf5a679b043c2762c17e841c9
Andy Edwards <jedwards@fastmail.com>
2017-06-09 23:53:33 -0500

    fix(complete.sh): fix bug with checking out master branch
    and fix bug with upstream of master being skeleton

--- complete.sh
+++ complete.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ $(git symbolic-ref --short HEAD) != 'master' ]]; then
+  git branch -D master
+  git checkout -b master
+fi
 git remote rename origin skeleton
-git branch -D master
-git checkout -b master
+git remote add origin $(node -p 'require("./package").repository.url')
+git branch --set-upstream master origin/master
 rm complete.sh
 git add --all .
 git commit -n -m 'pollinate project'
+
