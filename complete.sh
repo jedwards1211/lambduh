308117bee13f94c8705c45658732c314ab1cd4d5
Andy Edwards <jedwards@fastmail.com>
2017-06-09 23:58:25 -0500

    fix(complete.sh): unset upstream for master on pollinate

--- complete.sh
+++ complete.sh
@@ -5,9 +5,9 @@ if [[ $(git symbolic-ref --short HEAD) != 'master' ]]; then
   git branch -D master
   git checkout -b master
 fi
+git branch --unset-upstream master
 git remote rename origin skeleton
 git remote add origin $(node -p 'require("./package").repository.url')
-git branch --set-upstream master origin/master
 rm complete.sh
 git add --all .
 git commit -n -m 'pollinate project'
