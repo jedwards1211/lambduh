8f24385d6cfd9e52038b965279efb4ab77e0d411
Andy Edwards <jedwards@fastmail.com>
2017-06-14 12:53:51 -0500

    fix(package): fix various issues and add tests

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
-## transform `stdin` with ES6 lambda instead of using `sed`, `jq`, etc. Duh!
+## transform `stdin` with JS function instead of using `sed`, `jq`, etc. Duh!
 
 My reaction to `jq`: 🤔 *This is cool, but I love ES6. Isn't pure ES6 good enough?*
 
@@ -31,15 +31,22 @@ Edwards, Andy
 Doe, John
 ```
 
+## Node version note
+
+The syntax that `duh` accepts for your function depends on the version of Node you're running.  If you're running a
+really old version, you may not be able to use lambdas, and will have to resort to `function () { return ... }` instead.
+
 ## Command
 
 You can use `lambduh` or just `duh`.
 
 ## Modes
 
-You may have noticed above that you can apply a transformation to each line.
+You may have noticed above that you can apply a transformation to each line of a plain text file.
 This is pretty much the only magic built into the `duh` command:
-* If the first argument starts with `l` or `L`, it runs in **line mode**.
+* If the first argument of your function is `lines` (ignoring case), it runs in **lines mode**.
+* Otherwise if the first argument of your function starts with `l` or `L`, it runs in **line mode**.
+* If the first argument of your function starts with `t` or `T`, it runs in **text mode**.
 * Otherwise, it runs in **json mode**.
 
 :star: :+1: **Nice!!** :punch: :sparkles:
@@ -56,7 +63,17 @@ to `stdout`, except:
 * If your function returns `false`, it quits reading lines.
 * If your function returns `null` or `undefined`, it skips to the next line without printing anything.
 
-## Comes with lodash functions built in
+### Lines Mode
+
+Calls your function with an array of lines from the file.  If your function returns an array of lines, it writes them
+in order to `stdout`.  Otherwise, it calls `String` on it and writes that string to `stdout`.
+
+### Text Mode
+
+Calls your function with the entire text of the file in a single string, and writes what your function returns to
+`stdout`.
+
+## Comes with lodash built in
 ```
 > curl 'https://api.github.com/repos/stedolan/jq/commits?per_page=2' \
   | duh 'json => json.map(c => _.pick(c, "sha", "commit.message"))'
--- test/index.js
+++ test/index.js
@@ -1,6 +1,173 @@
-describe('test setup', function () {
-  it('works', function () {
+var spawn = require('child_process').spawn
+var expect = require('chai').expect
+var pickInputMode = require('../lib/pickInputMode')
 
+function testCase(input, fn, output, done) {
+  var child = spawn(process.argv[0], [
+    require.resolve('../lib'),
+    fn,
+  ])
+  var outchunks = []
+  var errchunks = []
+  child.stdout.on('data', function (chunk) { outchunks.push(chunk) })
+  child.stderr.on('data', function (chunk) { errchunks.push(chunk) })
+  child.on('close', function (code, signal) {
+    expect(signal).not.to.exist
+    if (output instanceof Error) {
+      expect(code).not.to.equal(0)
+      expect(errchunks.join('')).to.match(new RegExp("Error: " + output.message))
+    } else {
+      expect(errchunks.join('')).to.equal('')
+      expect(outchunks.join('')).to.equal(output)
+      expect(code).to.equal(0)
+    }
+    done()
+  })
+
+  child.stdin.write(input)
+  child.stdin.end()
+}
+
+describe('lambduh', function () {
+  it("errors if first argument isn't a function", function (done) {
+    testCase(
+      'input',
+      '2881234',
+      new Error("first argument must evaluate to a function"),
+      done
+    )
+  })
+  it("errors if it can't determine input mode", function (done) {
+    testCase(
+      'input',
+      '() => {}',
+      new Error("Can't determine input mode!"),
+      done
+    )
+  })
+  describe('pickInputMode', function () {
+    it('works for basic lambda', function () {
+      expect(pickInputMode(' lines => lines.reverse()')).to.equal('lines')
+      expect(pickInputMode('line => line.toUpperCase()')).to.equal('line')
+      expect(pickInputMode('t => t.toUpperCase()')).to.equal('text')
+    })
+    it('works for lambda with parentheses', function () {
+      expect(pickInputMode(' (lines) => lines.reverse()')).to.equal('lines')
+      expect(pickInputMode('( line) => line.toUpperCase()')).to.equal('line')
+      expect(pickInputMode(' ( t ) => t.toUpperCase()')).to.equal('text')
+    })
+    it('works for function without name', function () {
+      expect(pickInputMode('function (lines) { return lines.reverse() }')).to.equal('lines')
+      expect(pickInputMode(' function ( line) { return line.toUpperCase() }')).to.equal('line')
+      expect(pickInputMode('function( t ) { return t.toUpperCase() }')).to.equal('text')
+      expect(pickInputMode('function(j){ return j.toUpperCase() }')).to.equal('json')
+    })
+    it('works for function with name', function () {
+      expect(pickInputMode('function blah_(lines) { return lines.reverse() }')).to.equal('lines')
+      expect(pickInputMode(' function bl_ah ( line) { return line.toUpperCase() }')).to.equal('line')
+      expect(pickInputMode('function aksnd923ka_( t ) { return t.toUpperCase() }')).to.equal('text')
+      expect(pickInputMode('function blaAARrgh__(j){ return j.toUpperCase() }')).to.equal('json')
+    })
+    it("returns null if it can't find first argument", function () {
+      expect(pickInputMode('(')).to.equal(null)
+    })
+  })
+  describe('jsonMode', function () {
+    it('works for valid input', function (done) {
+      var input = [{"hash": "892n3kjasdf82", "name": "Andy Edwards"}, {"hash": "8nkkk0238lksd", "name": "John Doe"}]
+      testCase(
+        JSON.stringify(input),
+        'json => json.map(c => c.hash)',
+        JSON.stringify(input.map(function (c) { return c.hash }), null, 2),
+        done
+      )
+    })
+    it('has access to lodash', function (done) {
+      var input = [{"hash": "892n3kjasdf82", "name": "Andy Edwards"}, {"hash": "8nkkk0238lksd", "name": "John Doe"}]
+      testCase(
+        JSON.stringify(input),
+        'json => _.map(json, c => c.hash)',
+        JSON.stringify(input.map(function (c) { return c.hash }), null, 2),
+        done
+      )
+    })
+    it('errors for invalid input', function (done) {
+      testCase(
+        '[{"hash": "892n3kjasdf82", "name": "Andy Edwards"}, {"hash": "8nkkk0238lksd", "name": "',
+        'json => json.map(c => c.hash)',
+        new Error("Unexpected end of JSON input"),
+        done
+      )
+    })
+  })
+  describe('lineMode', function () {
+    it('works for valid input', function (done) {
+      var names = ['Billy Bob', 'Jim Bob', 'Ida Mae', 'Thelma Louise']
+      testCase(
+        names.join('\n') + '\n',
+        'line => line.split(/\\s+/).reverse().join(", ")',
+        ['Bob, Billy', 'Bob, Jim', 'Mae, Ida', 'Louise, Thelma'].join('\n') + '\n',
+        done
+      )
+    })
+    it('passes index to function', function (done) {
+      var lines = ['a', 'b', 'c']
+      testCase(
+        lines.join('\n') + '\n',
+        '(line, index) => index + ": " + line',
+        ['0: a', '1: b', '2: c'].join('\n') + '\n',
+        done
+      )
+    })
+    it('handles errors in function', function (done) {
+      var lines = ['a', 'b', 'c']
+      testCase(
+        lines.join('\n') + '\n',
+        'line => { throw new Error("TEST") }',
+        new Error("TEST"),
+        done
+      )
+    })
+  })
+  describe('linesMode', function () {
+    it('works for valid input', function (done) {
+      var names = ['Billy Bob', 'Jim Bob', 'Ida Mae', 'Thelma Louise']
+      testCase(
+        names.join('\n') + '\n',
+        'lines => lines.reverse()',
+        names.reverse().join('\n') + '\n',
+        done
+      )
+    })
+    it('works when function returns non-Array', function (done) {
+      var names = ['Billy Bob', 'Jim Bob', 'Ida Mae', 'Thelma Louise']
+      testCase(
+        names.join('\n') + '\n',
+        'lines => lines.join("\\t")',
+        names.join('\t'),
+        done
+      )
+    })
+  })
+  describe('textMode', function () {
+    it('works for valid input', function (done) {
+      var names = ['Billy Bob', 'Jim Bob', 'Ida Mae', 'Thelma Louise']
+      testCase(
+        names.join('\n') + '\n',
+        'text => text.toUpperCase()',
+        (names.join('\n') + '\n').toUpperCase(),
+        done
+      )
+    })
+    it('handles errors in function', function (done) {
+      var lines = ['a', 'b', 'c']
+      testCase(
+        lines.join('\n') + '\n',
+        'text => { throw new Error("TEST") }',
+        new Error("TEST"),
+        done
+      )
+    })
   })
 })
 
